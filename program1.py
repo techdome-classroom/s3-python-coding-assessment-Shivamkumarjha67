084bca8baa36b437721775794fba40b926f72544
Shivam Kumar Jha <shkrjh@gmail.com>
2024-10-08 15:28:26 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,7 +4,31 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+
+        for ch in s:
+            if ch in '({[':
+                stack.append(ch)
+            else:
+                if len(stack) < 1:
+                    return False
+                if not self.check(stack, ch):
+                    return False
+
+        return len(stack) == 0
+    
+    def check(self, stack, c):
+        if stack[-1] == '(' and c == ')':
+            stack.pop()
+            return True
+        elif stack[-1] == '[' and c == ']':
+            stack.pop()
+            return True
+        elif stack[-1] == '{' and c == '}':
+            stack.pop()
+            return True
+
+        return False
 
 
 
